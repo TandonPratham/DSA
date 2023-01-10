dda92edebd55cd79652b1e56747c9eac877d6591
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2023-01-10 19:14:54 +0530

    Update _4.py

--- _4.py
+++ _4.py
@@ -1,5 +1,6 @@
+string = input('give the string:')
 def nonRepeat():
-    string = input('give the string:')
+    
     for i in string:
         if (string.find(i, (string.find(i)+1))) == -1:
             print("First non-repeating character is", i)
